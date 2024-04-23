4b51d641187fe80f345e15588326af272f6e4193
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-04-23 14:44:52 +0600

    added Project Setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors'
+
+const app: Application = express();
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+      Message:"Ph Healthcare Server"
+  })  
+})
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-
-const app = express();
+import { Server } from 'http';
+import app from './app';
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log("APp is Listhening on port 3000")
-})
+async function main() {
+    const server: Server = app.listen(port, () => {
+        console.log("server listening on port " + port);
+    })
+}
+main()
